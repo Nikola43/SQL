96a190e9d4e2e58c1a3cc3cf568c7d943ce5b7eb
Paulo Soares <pauloxti@gmail.com>
2016-12-01 12:37:34 +0100

    examen2015

---
+++ Examen2015/Examen2015.sql
@@ -0,0 +1,18 @@
+--Partido: Ideología, Nombre lider
+--Coalición: Ambito
+--Formación: ID, Nombre completo, nombre abreviado, fecha de fundación
+--Interventor: Nombre, Apellidos, DNI, Fecha Nacimiento
+--Apoderado: Nombre, Apellidos, DNI, Fecha Nacimiento
+--Mesa: Numero distrito, numero sección, letra mesa, Direcciones (es un campo para introducir
+--un texto de hasta 1200 caracteres), Numero electores
+--Candidatura: ID, Nombre, Nombre del Representante, Apellidos del representante, telefono
+--Compone: Numero orden
+--Circunscripcion: Codigo, Nombre
+--Candidato: Nombre, Apellidos, DNI, Fecha Nacimiento, orden elección
+--Vota Congreso: Numero votos (se rellena tras el escrutinio)
+--Vota Senado: Numero votos (se rellena tras el escrutinio)
+
+CREATE DATABASE PoliticaIncompetente
+Go
+USE PoliticaIncompetente
+Go
