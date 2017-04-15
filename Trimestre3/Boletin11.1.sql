85e7deafa1c62241b1d5482d268ddc21d1c8b07c
Paulo Soares <pauloxti@gmail.com>
2017-04-15 19:55:34 +0200

    autoCommit

--- Trimestre3/Boletin11.1.sql
+++ Trimestre3/Boletin11.1.sql
@@ -21,7 +21,7 @@ ELSE
         PRINT 'Ese producto no existe en la base de datos, procedemos a insertarlo'
         BEGIN TRANSACTION
             INSERT INTO Products
-            VALUES ('Cruzcampo lata', 16, 1, 'Pack 6 latas', 4.3, 7, NULL, NULL,NULL)
+            VALUES ('Cruzcampo lata', 16, 1, 'Pack 6 latas', 4.3, 7,6,6,6)
         COMMIT TRANSACTION
     END
 
@@ -33,32 +33,134 @@ SELECT * FROM Products
       Si no existe, créala*/
 
     DECLARE @nombreTabla NVARCHAR(20)
-    SET @nombreTabla = 'ProductSale';
+    SET @nombreTabla = 'ProductSales';
 
-    IF ((SELECT dbo.comprobarSiTablaExiste(@nombreTabla)) = 1)
+    IF OBJECT_ID (@nombreTabla) IS NOT NULL
         BEGIN
-            PRINT 'existe'
+            PRINT 'La tabla: '+@nombreTabla+' ya existe.'
         END
     ELSE
         BEGIN
-            PRINT 'No existe'
+            PRINT 'La tabla: '+@nombreTabla+' no existe, procederemos a crearla.'
+
+            CREATE TABLE ProductSales
+            (
+                -- Columnas
+                ProductID INT NOT NULL CONSTRAINT FK_ProductID FOREIGN KEY REFERENCES Products(ProductID)
+                ON DELETE CASCADE ON UPDATE CASCADE,
+                ProductName nvarchar(40),
+                UnitPrice money NULL,
+                [Unidades vendidas] int NULL,
+                [Total facturado] money NULL
+            )
+
+            -- Insertamos los datos
+            INSERT INTO ProductSales
+            SELECT P.ProductID, P.ProductName, P.UnitPrice, SUM(OD.Quantity) AS [Unidades vendidas], P.UnitPrice*SUM(OD.Quantity) AS [Total facturado]
+            FROM Products AS P
+            INNER JOIN [Order Details] AS OD
+            ON P.ProductID = OD.ProductID
+            GROUP BY P.ProductID,P.ProductName,P.UnitPrice
+
+            -- Comprobamos si la tabla fue creada correctamente
+            IF OBJECT_ID (@nombreTabla) IS NOT NULL
+                BEGIN
+                    PRINT 'La tabla: '+@nombreTabla+' fue creada correctamente.'
+                END
+            ELSE
+                BEGIN
+                    PRINT 'La tabla: '+@nombreTabla+' no pudo crearse correctamente.'
+                    SELECT * FROM ProductSales
+                END
         END
 
 /* 3. Comprueba si existe una tabla llamada ShipShip. Esta tabla ha de tener de cada Transportista el ID,
       el Nombre de la compañía, el número total de envíos que ha efectuado y el número de países diferentes
       a los que ha llevado cosas. Si no existe, créala */
-      IF ((SELECT dbo.comprobarSiTablaExiste('ShipShip')) = '1')
-          BEGIN
-              PRINT 'existe'
-          END
-      ELSE
-          BEGIN
-              PRINT 'No existe'
-          END
+
+    DECLARE @nombreTabla NVARCHAR(20)
+    SET @nombreTabla = 'ShipShip';
+
+    IF OBJECT_ID (@nombreTabla) IS NOT NULL
+        BEGIN
+            PRINT 'La tabla: '+@nombreTabla+' ya existe.'
+        END
+    ELSE
+        BEGIN
+            PRINT 'La tabla: '+@nombreTabla+' no existe, procederemos a crearla.'
+
+            CREATE TABLE ShipShip
+            (
+                ID_Transportista int,
+                CompanyName VARCHAR(40),
+                Numero_Total_Envios int,
+                Numero_Paises_Provee int,
+            )
+
+            IF OBJECT_ID (@nombreTabla) IS NOT NULL
+                BEGIN
+                    PRINT 'La tabla: '+@nombreTabla+' fue creada correctamente.'
+                END
+            ELSE
+                BEGIN
+                    PRINT 'La tabla: '+@nombreTabla+' no pudo crearse correctamente.'
+                    SELECT * FROM ProductSales
+                END
+        END
+
+
+    SELECT S.ShipperID
+    FROM Shippers AS S
+
 
 /* 4. Comprueba si existe una tabla llamada EmployeeSales. Esta tabla ha de tener de cada empleado su ID,
       el Nombre completo, el número de ventas totales que ha realizado, el número de clientes diferentes a los
       que ha vendido y el total de dinero facturado. Si no existe, créala */
+
+    DECLARE @nombreTabla NVARCHAR(20)
+    SET @nombreTabla = 'EmployeeSales';
+
+    -- Si la tabla existe
+    IF OBJECT_ID (@nombreTabla) IS NOT NULL
+        BEGIN
+            PRINT 'La tabla: '+@nombreTabla+' ya existe.'
+        END
+    ELSE
+        BEGIN
+            PRINT 'La tabla: '+@nombreTabla+' no existe, procederemos a crearla.'
+
+            -- Sino existe la creamos
+            CREATE TABLE EmployeeSales
+            (
+                EmployeeID INT NOT NULL CONSTRAINT FK_Employee FOREIGN KEY REFERENCES Employees(EmployeeID)
+                ON DELETE CASCADE ON UPDATE CASCADE,
+                Nombre_Completo VARCHAR(30),
+                Numero_Ventas_Totales INT,
+                Numero_Clientes_Diferentes INT,
+                Total_Dinero_Facturado MONEY
+            )
+
+            -- Insertamos datos
+            INSERT INTO EmployeeSales
+            SELECT E.EmployeeID, E.LastName+' '+E.FirstName, COUNT(O.EmployeeID) AS Numero_Ventas_Totales, COUNT(DISTINCT CustomerID) AS Numero_Clientes_Diferentes, SUM(OD.UnitPrice * OD.Quantity)
+            FROM Employees AS E
+            INNER JOIN Orders AS O
+            ON E.EmployeeID = O.EmployeeID
+            INNER JOIN [Order Details] AS OD
+            ON O.OrderID = OD.OrderID
+            GROUP BY E.EmployeeID, E.LastName, E.FirstName
+
+            -- Comprobamos si todo fue bien
+            IF OBJECT_ID (@nombreTabla) IS NOT NULL
+                BEGIN
+                    PRINT 'La tabla: '+@nombreTabla+' fue creada correctamente.'
+                END
+            ELSE
+                BEGIN
+                    PRINT 'La tabla: '+@nombreTabla+' no pudo crearse correctamente.'
+                    SELECT * FROM EmployeeSales
+                END
+        END
 
 /* 5. Entre los años 96 y 97 hay productos que han aumentado sus ventas y otros que las han disminuido.
       Queremos cambiar el precio unitario según la siguiente tabla:
@@ -70,30 +172,63 @@ SELECT * FROM Products
       * Mayor del 50%    -      10% con un máximo de 2,25
 */
 
-GO
-ALTER FUNCTION dbo.comprobarSiTablaExiste(@nombreTabla NVARCHAR(20))
-    RETURNS BIT
-    BEGIN
-        DECLARE @existe BIT
-        SET @existe = 0;
 
-        IF OBJECT_ID (@nombreTabla) IS NOT NULL
-            SET @existe = 1;
 
-        RETURN @existe
-    END
-GO
+ UPDATE Products
+ SET unitPrice =
+ CASE
+    -- cuando las ventas del 97 son negativas
+    WHEN ventas9697 < 0
+		    THEN (UnitPrice-(UnitPrice*0.10))   -- entonces actualizamos su precio en un 10% menos
 
-SELECT dbo.comprobarSiTablaExiste('Employees')
+    -- cuando las ventas del 97 son entre el 0 y 10%
+	  WHEN ventas9697 >= 0 AND ventas9697<(Vendido96*0.10)
+		    THEN (UnitPrice = UnitPrice)	    -- entonces actualizamos su precio en un 10% menos
 
-GO
-      IF ( (dbo.comprobarSiTablaExiste('Employees')) = 1)
-          BEGIN
-              PRINT 'existe'
+	  WHEN ventas9697>=(Vendido96*0.10) AND ventas9697<(vendido96*0.50)
+	      THEN (UnitPrice*1.05)
 
-          END
-      ELSE
-          BEGIN
-              PRINT 'No existe'
+	WHEN ventas9697>=(Vendido96*0.50)
+    THEN
+		    CASE
+			      WHEN (UnitPrice*0.10)<2.25
+                THEN (UnitPrice+ (UnitPrice*0.10))
+			      ELSE
+			          (UnitPrice+ 2.25)
+			  END
+ END
+	from ventasEntre9697
 
-          END
+
+SELECT * FROM VentasEntre9697
+
+ -- Vista diferencia 9697
+ ALTER VIEW diferenciaVentasEntre9697 AS
+ SELECT  OD.ProductID, (SUM(V97.cantidadVendida)-SUM(V96.cantidadVendida)) as ventas9697
+ FROM [Order Details] AS OD
+ INNER JOIN Ventas96 AS V96
+   ON V96.ProductID= OD.ProductID
+ INNER JOIN Ventas97 AS V97
+   ON	V97.ProductID = OD.ProductID
+ GROUP BY OD.ProductID
+
+-- Vista 96
+ALTER VIEW Ventas96 AS
+SELECT ProductId,(Quantity) AS CantidadVendida
+FROM Orders AS O
+INNER JOIN [Order Details] AS OD
+ON O.OrderID = OD.OrderID
+WHERE YEAR(OrderDate) = 1996
+
+-- Vista 97
+ALTER VIEW Ventas97 AS
+SELECT ProductId,(Quantity) AS CantidadVendida
+FROM Orders AS O
+INNER JOIN [Order Details] AS OD
+ON O.OrderID = OD.OrderID
+WHERE YEAR(OrderDate) = 1997
+
+
+SELECT * FROM Ventas97
+SELECT * FROM Ventas96
+SELECT * FROM diferenciaVentasEntre9697
