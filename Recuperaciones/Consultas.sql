191ae545d427b9c1fedca09d0948cf25d73fb3c4
Paulo Soares <pauloxti@gmail.com>
2017-06-01 14:36:30 +0200

    sql recuperacion

---
+++ Recuperaciones/Consultas.sql
@@ -0,0 +1,121 @@
+USE ICOTR
+GO
+SET DATEFORMAT YMD
+GO 
+
+--Consultas
+--Escribe el código SQL necesario para realizar las siguientes operaciones sobre la base de datos ICOTR.
+--Cuando la consulta sea muy compleja usa vistas para realizar partes de la misma
+--Consultas con agregados
+
+--1.Número de veces que se ha pedido cada topping
+SELECT COUNT(T.ID) AS [Veces que se ha pedido]
+FROM ICPedidos AS P
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+INNER JOIN ICHeladosToppings AS HT
+ON H.ID = HT.IDHelado
+INNER JOIN ICToppings AS T
+ON HT.IDTopping = T.ID
+GROUP BY T.ID
+
+--2.Número de veces que se ha pedido cada topping cada año
+SELECT COUNT(T.ID) AS [Veces que se ha pedido], YEAR(P.Recibido) AS AÑO
+FROM ICPedidos AS P
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+INNER JOIN ICHeladosToppings AS HT
+ON H.ID = HT.IDHelado
+INNER JOIN ICToppings AS T
+ON HT.IDTopping = T.ID
+GROUP BY YEAR(P.Recibido)
+
+--3.Número de veces que se ha pedido cada topping en cada establecimiento
+SELECT COUNT(T.ID) AS [Veces que se ha pedido], P.IDEstablecimiento
+FROM ICPedidos AS P
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+INNER JOIN ICHeladosToppings AS HT
+ON H.ID = HT.IDHelado
+INNER JOIN ICToppings AS T
+ON HT.IDTopping = T.ID
+GROUP BY P.IDEstablecimiento
+
+
+-- REVISAR --
+--4.Número de pedidos que incluyen cada topping. OJO: Un pedido puede incluir el mismo topping más de una vez
+SELECT COUNT(DISTINCT P.ID) AS [Veces que se ha pedido], T.Topping
+FROM ICPedidos AS P
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+INNER JOIN ICHeladosToppings AS HT
+ON H.ID = HT.IDHelado
+INNER JOIN ICToppings AS T
+ON HT.IDTopping = T.ID
+GROUP BY T.Topping
+
+--5.Número de helados repartidos por cada repartidor en el año 2014.
+SELECT COUNT(H.ID) AS [Helados vendidos], P.IDRepartidor
+FROM ICPedidos AS P
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+INNER JOIN ICHeladosToppings AS HT
+ON H.ID = HT.IDHelado
+INNER JOIN ICToppings AS T
+ON HT.IDTopping = T.ID
+WHERE YEAR(P.Recibido) = 2014 
+GROUP BY P.IDRepartidor
+
+--6.Número de pedidos y cantidad facturada por cada repartidor en el año 2014.
+SELECT COUNT(P.ID) AS [Numero Pedidos], SUM(P.Importe) AS [Cantidad Facturada], P.IDRepartidor
+FROM ICPedidos AS P
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+INNER JOIN ICHeladosToppings AS HT
+ON H.ID = HT.IDHelado
+INNER JOIN ICToppings AS T
+ON HT.IDTopping = T.ID
+WHERE YEAR(P.Recibido) = 2014 
+GROUP BY P.IDRepartidor
+
+--7.Pedidos (ID, Nombre y apellidos del cliente, nombre del establecimiento e importe) que incluyan el sabor menos vendido de cada ciudad. La ciudad se tomará del establecimiento.
+SELECT TOP 10 P.ID, C.Nombre, C.Apellidos, E.Denominacion, P.Importe, COUNT(H.Sabor) AS [Veces vendidas], E.Ciudad
+FROM ICEstablecimientos AS E
+INNER JOIN ICPedidos AS P
+ON E.ID = P.IDEstablecimiento
+INNER JOIN ICClientes AS C
+ON P.IDCliente = C.ID
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+GROUP BY P.ID, C.Nombre, C.Apellidos, E.Denominacion, P.Importe, E.Ciudad
+ORDER BY COUNT(H.Sabor) ASC
+
+-- REVISAR --
+--8.Sabor más vendido en cada establecimiento. Incluir cuantos pedidos incluyen ese sabor. Ten en cuenta que en un pedido hay varios helados y varios pueden tener el mismo sabor.
+SELECT DISTINCT H.Sabor, COUNT(H.Sabor) AS [Veces vendidas]
+FROM ICEstablecimientos AS E
+INNER JOIN ICPedidos AS P
+ON E.ID = P.IDEstablecimiento
+INNER JOIN ICClientes AS C
+ON P.IDCliente = C.ID
+INNER JOIN ICHelados AS H
+ON P.ID = H.IDPedido
+GROUP BY H.Sabor, E.Denominacion
+ORDER BY COUNT(H.Sabor)
+
+--9.Cifra total de ventas de cada establecimiento en cada estación del año. 
+--El invierno va del 21 de diciembre al 21 de marzo, la primavera hasta en 21 de junio, el verano termina el 21 de septiembre.
+SELECT SUM(P.Importe), E.Denominacion
+FROM ICEstablecimientos AS E
+INNER JOIN ICPedidos AS P
+ON E.ID = P.IDEstablecimiento
+GROUP BY E.Denominacion
+
+
+
+--10.Porcentaje de helados que usan cada tipo de contenedor
+--11.Otras cuestiones
+
+--12.Añade a la tabla clientes una columna de tipo DECIMAL (3,2) llamada TipoDescuento. No admitirá valores nulos y su valor por defecto es 0.00. El valor de esta columna indicará el tipo de descuento que hacemos a los clientes en sus pedidos en tanto por uno.
+--13.Añade una restricción para que el valor de esa columna tenga que estar entre 0 y 0.3
+--14.Rellena la columna sumando 0.02 a todos los clientes que hayan hecho más de tres pedidos durante los meses de octubre a febrero.
---
+++ Recuperaciones/ConsultasExtra.sql
@@ -0,0 +1,20 @@
+Extra: Consultas
+Resuelve el reto que te propone este enlace con los siguientes cambios:
+
+Modifica el script de creación para usar una tabla en lugar de una variable tipo tabla.
+
+Pista: Usa SELECT - INTO en lugar de DECLARE - INSERT
+Una vez tengas el SELECT que resuelve el problema, transfórmalo en una función a la que se pase como parámetro el número que estamos buscando y nos devuelva el más cercano.
+
+ 
+
+Si te ha sabido a poco, aquí tienes otro que pondrá a prueba tu habilidad para construir consultas en lugar de procesar los datos por filas.
+
+Última modificación: viernes, 7 de junio de 2013, 09:03
+
+
+http://beyondrelational.com/modules/19/tsql-beginners/305/tsql-beginners-challenge-5-search-the-closest-number-in-the-series.aspx?tab=info
+
+http://beyondrelational.com/modules/19/tsql-beginners/297/tsql-beginners-challenge-1-find-the-second-highest-salary-for-each-department.aspx?tab=info
+
+
